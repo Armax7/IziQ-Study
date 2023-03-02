9b6cbc377dec79ad841d4d728865aef41a9b3d63
Armando Isaac Garcia <armando7jx@gmail.com>
2023-03-02 11:10:31 -0600

    NextJS 13 experimetal repository initial commit

---
+++ app/about/page.jsx
@@ -0,0 +1,8 @@
+
+function AboutPage () {
+    return (
+        <h1>About Page</h1>
+    )
+}
+
+export default AboutPage
---
+++ app/page.jsx
@@ -0,0 +1,8 @@
+
+function LandingPage () {
+    return (
+        <h1>Landing Page</h1>
+    )
+}
+
+export default LandingPage
