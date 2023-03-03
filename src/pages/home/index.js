5b3692ee76491e739295868ac42dafda15d85402
Armando Isaac Garcia <armando7jx@gmail.com>
2023-03-03 12:22:49 -0600

    Repo Restructure NextJS 12.1.0

---
+++ src/pages/home/index.js
@@ -0,0 +1,9 @@
+
+
+function Home() {
+  return (
+    <h1>Esto es Home</h1>
+  )
+}
+
+export default Home
---
+++ src/pages/index.js
@@ -0,0 +1,9 @@
+
+export default function Landing() {
+
+  return (
+    <div>
+      <h1>Esto es el Landing</h1>
+    </div>
+  );
+}
