271fb31b92c35e42ff369a21a91be45684d3140c
Bach16 <gwyn56717@gmail.com>
2023-03-04 17:57:28 -0500

    add UX styles

--- src/components/DeckContainer/Feature.jsx
+++ src/components/DeckContainer/Feature.jsx
@@ -3,7 +3,7 @@ import * as Chakra from "@chakra-ui/react";
 function Feature({ name, description,total_cards, ...rest }) {
    
     return (
-        <Chakra.Box p={7} boxShadow='md' id={name} backgroundColor="#ffffff"  maxWidth="400px"  borderWidth='1px' {...rest}  >
+        <Chakra.Box p={7} boxShadow='md' id={name} backgroundColor="#ffffff" transition="box-shadow.3s ease, transform .2s ease" _hover={{boxShadow: "lg"}} maxWidth="400px" cursor="pointer" borderWidth='1px' {...rest}  >
             <Chakra.Heading fontSize='xl'>{name}</Chakra.Heading>
             <Chakra.Text mt={3}>{description}</Chakra.Text>
             <Chakra.Tag size="sm"mt={3} variant='solid'backgroundColor="#5C66BB" colorScheme='teal'>{total_cards} cards</Chakra.Tag>
