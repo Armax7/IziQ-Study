e778c4df98de096e00b28662e4fc3a8c23b615d8
MatVaz14 <mativazquez-29@outlook.com>
2023-03-04 15:12:44 -0300

    in navigation remove some tags inside the nav, (but from the first commit), don't modify navbar from the development branch

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,34 +1,7 @@
 import Link from "next/link";
 
-const links =[
-    {
-        label: 'Landing',
-        route: '/',
-    },
-    {
-        label: 'Home',
-        route: '/home',
-    },
-    {
-        label: 'About',
-        route: '/about',
-    },
-]
-
 function Navigation() {
-  return (
-    <nav>
-        <ul>
-            {
-                links.map(({label,route}) => (
-                    <li key={route} >
-                        <Link href={route} >{label}</Link>
-                    </li>
-                ))
-            }
-        </ul>
-    </nav>  
-  )
+  return <nav></nav>;
 }
 
-export default Navigation
+export default Navigation;
