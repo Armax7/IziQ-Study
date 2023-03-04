0d575f4e6f71f9e1d8a39647f33b95256d7e259c
Armando Isaac Garcia <armando7jx@gmail.com>
2023-03-03 20:31:44 -0600

    Indexed Dropodown component

--- src/components/index.js
+++ src/components/index.js
@@ -1,1 +1,5 @@
-export {default as Navigation} from './Navigation/Navigation';
+export { default as Navigation } from "./Navigation/Navigation";
+export { default as FormSignUp } from "./Forms/FormSignUp";
+export { default as FormLogin } from "./Forms/FormLogin";
+export { default as SearchBar } from "./SearchBar/SearchBar";
+export { default as Dropdown } from "./Dropdown/Dropdown";
