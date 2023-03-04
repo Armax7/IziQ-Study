1839e4bb6d95797bf805fc0d27effe0766911f69
MatVaz14 <mativazquez-29@outlook.com>
2023-03-04 15:04:15 -0300

    modified index.js, Added img/text/buttons in LandingPage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,18 @@
+import style from "../styles/index.module.css";
 
 export default function Landing() {
-
   return (
-    <div>
-      <h1>Esto es el Landing</h1>
+    <div className={style.containerSections}>
+      <section className={style.sectionOne}>
+        <div className={style.sectionOne_one}></div>
+        <div className={style.sectionOne_two}>
+          <div className={style.containerOne_twoData}>
+            <h1>Learn Easy</h1>
+            <h3>Acquire knowledge with flash-cards</h3>
+            <button>Join Now</button>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
