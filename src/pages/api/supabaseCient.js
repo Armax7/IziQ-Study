151a45f01f7f14e69b57da6e3ed2b7ddeafd3597
jeromanp <jeduardo.roman69@gmail.com>
2023-03-04 00:01:03 -0600

    rename file supabaseClient

--- src/pages/api/supabaseCient.js
+++
@@ -1,6 +0,0 @@
-import { cliente, createClient } from '@supabase/supabase-js'
-
-const supabaseURL = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_PUBLIC_ANON_KEY;
-
-export const supabase = createClient(supabaseURL, supabaseAnonKey);
