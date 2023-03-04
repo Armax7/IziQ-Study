7aa3894181991aaef3de288f0fd6a935efcb6a6f
Armando Isaac Garcia <armando7jx@gmail.com>
2023-03-03 20:41:20 -0600

    Changed index.js to match local repo requirements

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,2 @@
 export { default as Navigation } from "./Navigation/Navigation";
-export { default as FormSignUp } from "./Forms/FormSignUp";
-export { default as FormLogin } from "./Forms/FormLogin";
-export { default as SearchBar } from "./SearchBar/SearchBar";
 export { default as Dropdown } from "./Dropdown/Dropdown";
