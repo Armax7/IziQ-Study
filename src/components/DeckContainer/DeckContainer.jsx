f3d1932a6859e38c182e373925bacc3cbd656cde
Armando Isaac Garcia <armando7jx@gmail.com>
2023-03-04 18:11:01 -0600

    Added if statement to mock secction in DeckContainer for security reasons

--- src/components/DeckContainer/DeckContainer.jsx
+++ src/components/DeckContainer/DeckContainer.jsx
@@ -4,47 +4,47 @@ import Feature from "./deckContainer_helpers/Feature";
 function DeckContainer({decks,num_decks}) {
     ///---------------Seccion mock para decks----------------
     ///borrar seccion cuando se implemente decks
-    const array = [
-        {
-            id:1,
-            name:"English",
-            description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit",
-            total_cards:5,
-            status:"learned",
-            user_id:2313551_5656,
-            category_id:321
-        },
-        {
-            id:2,
-            name:"French",
-            description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit",
-            total_cards:7,
-            status:"learned",
-            user_id:8984161561_5656,
-            category_id:321
-        },
-        {
-            id:3,
-            name:"Spanish",
-            description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit",
-            total_cards:25,
-            status:"learned",
-            user_id:55615445561_5656,
-            category_id:321
-        },
-        {
-            id:4,
-            name:"Arab",
-            description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit",
-            total_cards:25,
-            status:"learned",
-            user_id:55615445561_5656,
-            category_id:321
-        },
-        
-      
-    ]
-    decks = array
+    if (!decks){
+        const array = [
+            {
+                id:1,
+                name:"English",
+                description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+                total_cards:5,
+                status:"learned",
+                user_id:2313551_5656,
+                category_id:321
+            },
+            {
+                id:2,
+                name:"French",
+                description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+                total_cards:7,
+                status:"learned",
+                user_id:8984161561_5656,
+                category_id:321
+            },
+            {
+                id:3,
+                name:"Spanish",
+                description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+                total_cards:25,
+                status:"learned",
+                user_id:55615445561_5656,
+                category_id:321
+            },
+            {
+                id:4,
+                name:"Arab",
+                description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+                total_cards:25,
+                status:"learned",
+                user_id:55615445561_5656,
+                category_id:321
+            },
+        ]
+        decks = array
+    }
     ///---------------Termina seccion mock para decks----------------
   return (
     <Chakra.Stack spacing={8} direction='column' backgroundColor="#f2f2f2" margin="10px" justifyContent="space-evenly">
