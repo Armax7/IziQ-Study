ee1776396acf4b0aacedf48d75a202d9d8efcb83
jeromanp <jeduardo.roman69@gmail.com>
2023-03-04 22:32:00 -0600

    cambio nombre de componenete linea 5 de FormLoin a FormLogIn

--- src/components/index.js
+++ src/components/index.js
@@ -2,5 +2,5 @@ export { default as Navigation } from "./Navigation/Navigation";
 export { default as Dropdown } from "./Dropdown/Dropdown";
 export { default as CardContainer} from "./Card/CardContainer"
 export { default as FormSignUp } from "./Forms/FormSignUp";
-export { default as FormLogin } from "./Forms/FormLogin";
+export { default as FormLogIn } from "./Forms/FormLogIn";
 export { default as SearchBar } from "./SearchBar/SearchBar";
