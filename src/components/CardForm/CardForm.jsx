a2f5fd39c9e50b0358e0a1ddc291ac4da36c8a1b
Armando Isaac Garcia <armando7jx@gmail.com>
2023-03-07 14:18:00 -0600

    Fixed bug where cards were renderized on this component

--- src/components/CardForm/CardForm.jsx
+++ src/components/CardForm/CardForm.jsx
@@ -26,69 +26,11 @@ const CardForm = () => {
 
   const handleAddCard = () => {
     setCards([...cards, cards.length]);
+    console.log("Card added succesfully");
   };
 
   return (
     <>
-      {cards.map((cardIndex) => (
-        <div key={cardIndex}>
-          <Chakra.Box
-            mx="auto"
-            w="90%"
-            bg="#FFFFFF"
-            boxShadow="0px 4px 4px rgba(0, 0, 0, 0.25)"
-            borderRadius="15px"
-            fontFamily="Poppins"
-            fontStyle="normal"
-            fontWeight="600"
-            color="#000000"
-            mb="25px"
-          >
-            <Chakra.Heading
-              fontSize="28px"
-              lineHeight="42px"
-              display="inline-flex"
-            >
-            </Chakra.Heading>
-            <div className={styles.form_line}>
-              <Chakra.FormControl mr="30px" pt="25px">
-                <Chakra.Input
-                  type="text"
-                  name="question"
-                  id="question"
-                  value={formData.question}
-                  onChange={handleOnChange}
-                  variant="filled"
-                />
-                <Chakra.FormLabel color="#797979" mt="5px" htmlFor="question">
-                  Question
-                </Chakra.FormLabel>
-              </Chakra.FormControl>
-              <Chakra.FormControl mr="30px" pt="25px">
-                <Chakra.Input
-                  type="text"
-                  name="answer"
-                  id="answer"
-                  value={formData.answer}
-                  onChange={handleOnChange}
-                  variant="filled"
-                />
-                <Chakra.FormLabel color="#797979" mt="5px" htmlFor="answer">
-                  Answer
-                </Chakra.FormLabel>
-              </Chakra.FormControl>
-              <div className={styles.image_container}>
-                <div className={styles.image_column}>
-                  <div className={styles.image_icon}>
-                    <Image />
-                  </div>
-                  <div className={styles.image_text}>Image</div>
-                </div>
-              </div>
-            </div>
-          </Chakra.Box>
-        </div>
-      ))}
       <Chakra.Box
         textAlign="center"
         mx="auto"
