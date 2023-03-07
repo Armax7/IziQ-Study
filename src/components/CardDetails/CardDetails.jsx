0e739845c97f5cebdac30450032fd79dde50d82d
Bach16 <gwyn56717@gmail.com>
2023-03-07 15:32:40 -0500

    refactor CardDetails component

--- src/components/CardDetails/CardDetails.jsx
+++ src/components/CardDetails/CardDetails.jsx
@@ -1,24 +1,19 @@
 import * as Chakra from "@chakra-ui/react";
 import styles from "./CardDetails.module.css"
 import ImageIcon from "./CardDetails_helpers/ImageIcon"
+import {HiTrash} from "react-icons/hi"
+import {RiPencilFill} from "react-icons/ri"
 
 
-function CardDetails({id,question="how do you say apple in spanish?",answer="manzana",image,Deck_id,learned}) {
+function CardDetails({id,question="how do you say apple in spanish?",answer="manzana asd asd asd asdasd a sd",image,Deck_id,learned}) {
   return (
-    <Chakra.Flex   className={styles.card} backgroundColor="white" width="755px" margin="20px" borderRadius="10px" height="52px"  alignItems="center">
-      <Chakra.Editable className={styles.input}  defaultValue={question}>
-        <Chakra.EditablePreview  alignSelf="center" cursor="pointer" />
-        <Chakra.EditableInput className={styles.editableInput} _focus={{border:"none",textAlign:"center",color:"#242424"}} />
-      </Chakra.Editable>
-      <Chakra.Box height="30px" backgroundColor="#242424">
-        <Chakra.Divider orientation='vertical' backgroundColor="#242424"/>
-      </Chakra.Box>
-      <Chakra.Editable className={styles.input}   marginRight="10px"  defaultValue={answer}>
-        <Chakra.EditablePreview alignSelf="center" cursor="pointer"/>
-        <Chakra.EditableInput className={styles.editableInput} _focus={{border:"none",textAlign:"center",color:"#242424"}} />
-      </Chakra.Editable>
-      <ImageIcon/>
-      <Chakra.Button bg="#EB455F" padding="0px" _hover={{color: "#000000", backgroundColor:"#EBEBEB" }} _focus={{border:"none" }} minWidth="27px" marginLeft="10px" height="26px"  color="white">x</Chakra.Button>
+    <Chakra.Flex className={styles.card} flex="0 0 auto" padding="15px" backgroundColor="white"  margin="20px">
+      <Chakra.Text className={styles.input} borderRight="1px solid #c2c2c2">{question}</Chakra.Text>
+      <Chakra.Text className={styles.input}>{answer}</Chakra.Text>
+      <Chakra.Flex width="10%" justifyContent="space-between">
+        <RiPencilFill size="30px" cursor="pointer" />
+        <HiTrash size="30px" cursor="pointer" />
+      </Chakra.Flex>
     </Chakra.Flex>
   )
 }
