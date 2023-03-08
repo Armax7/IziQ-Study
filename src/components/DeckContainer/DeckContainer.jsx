c7d4f48443cb01fc9b420c3d8c48ef8489804031
Armando Isaac Garcia <armando7jx@gmail.com>
2023-03-08 10:35:16 -0600

    Synced file from development to remove it from PR

--- src/components/DeckContainer/DeckContainer.jsx
+++ src/components/DeckContainer/DeckContainer.jsx
@@ -2,11 +2,7 @@ import * as Chakra from "@chakra-ui/react";
 import Feature from "./deckContainer_helpers/Feature";
 import { DeckCover } from "../DeckCover/DeckCover";
 
-
-
 function DeckContainer({ decks }) {
-
-
   return (
     <Chakra.Stack
       spacing={8}
@@ -15,14 +11,9 @@ function DeckContainer({ decks }) {
       margin="10px"
       justifyContent="space-evenly"
     >
-      <Chakra.Heading fontSize="md" marginLeft="5%">
-        Recent Decks
-      </Chakra.Heading>
-      
       <Chakra.Box display="flex" justifyContent="space-evenly">
         <Chakra.Grid templateColumns="repeat(3, 1fr)" gap={6}>
           {decks?.map((deck) => (
-            ///Feature es Mock de DeckCover, reemplazar una vez exista el componente
             <DeckCover
               key={deck.id}
               name={deck.name}
@@ -30,13 +21,11 @@ function DeckContainer({ decks }) {
               total_cards={deck.total_cards}
               status={deck.status}
             />
-            ///Feature es Mock de DeckCover, reemplazar una vez exista el componente
           ))}
         </Chakra.Grid>
       </Chakra.Box>
     </Chakra.Stack>
   );
 }
-
 
 export default DeckContainer;
