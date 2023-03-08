620342b488572f7fb6e3ba70958a4a7a646ed266
unknown <jorgemathez14@gmail.com>
2023-03-07 21:09:20 -0300

    rendering of the decks of the logged in user

--- src/pages/decks/index.js
+++ src/pages/decks/index.js
@@ -1,16 +1,41 @@
-
 import * as Components from "../../components";
+import { supabase } from "../api/supabaseClient";
+import { useEffect, useState } from "react";
+import {  getAllDeck } from "../api/supabase_Helpers/supabase_Helper";
 
-function Decks() {
+const Decks = () => {
+ 
+  const [userID, setUserId] = useState("");
+  const [decks, setDecks] = useState([]);
+
+
+
+  useEffect(async () => {
+    const getIdUser = async () => {
+      const user = await supabase.auth.getUser();
+      const userID = user.data.user.id;
+      setUserId(userID);
+    };
+
+
+    getIdUser();
+
+
+    const decks = await getAllDeck();
+    setDecks(decks);
+  }, [userID]);
+
+
+  const deckFilter = decks.filter((e) => e.user_id === userID);
+
+  
+
+
   return (
     <div>
-    <Components.DeckContainer/>
-
-  <div>This is Decks route</div>
-  
-  </div>
-  
+      <Components.DeckContainer decks={deckFilter} />
+    </div>
   );
-}
+};
 
 export default Decks;
