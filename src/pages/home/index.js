cc09b7fc6a0719d3acea6e417a98747787f83ac2
jeromanp <jeduardo.roman69@gmail.com>
2023-03-07 18:30:25 -0600

    create states for userId and decks

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -3,6 +3,8 @@ import { useEffect, useState } from "react";
 import {  getAllDeck } from "../api/supabase_helper/supabase_helper";
 
 function Home() {
+  const [userID, setUserId] = useState("");
+  const [decks, setDecks] = useState([]);
   return (
     <>
       <h1>Esto es Home</h1>
