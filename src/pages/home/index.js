9f7b6db05b2a72c0257925d7821c8f374749e298
jeromanp <jeduardo.roman69@gmail.com>
2023-03-07 18:29:50 -0600

    import supabase, useState, useEffect, function getAllDeck

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,3 +1,6 @@
+import { supabase } from "../api/supabaseClient";
+import { useEffect, useState } from "react";
+import {  getAllDeck } from "../api/supabase_helper/supabase_helper";
 
 function Home() {
   return (
