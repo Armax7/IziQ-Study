4bd1243c713740ff0bd0d4943f5f50627780ac8e
jeromanp <jeduardo.roman69@gmail.com>
2023-03-08 11:09:35 -0600

    supabase post in user_details

--- src/pages/api/supabase_helpers/supabase_post.js
+++ src/pages/api/supabase_helpers/supabase_post.js
@@ -1,14 +1,21 @@
-import { supabase } from "../supabaseClient"
-
+import { supabase } from "../supabaseClient";
 
 export async function postUserDetails() {
-
+  try {
+    const { data, error } = await supabase
+      .from("users_details")
+      .eq("user_id", id)
+      .insert([
+        { alias: "user_alias" },
+        { birth_date: "user_birth_date" },
+        { gender: "user_gender" },
+        { occupation: "user_occupation" },
+      ]);
+    if (error) throw error;
+    return data;
+  } catch (error) {}
 }
 
-export async function postDeck() {
+export async function postDeck() {}
 
-}
-
-export async function postCard() {
-    
-}
+export async function postCard() {}
