5513df31d3c349e5df9e953e75463ca934dec179
jeromanp <jeduardo.roman69@gmail.com>
2023-03-08 12:37:16 -0600

    deleted insert to table categories/subcategories

--- src/pages/api/supabase_helpers/supabase_post.js
+++ src/pages/api/supabase_helpers/supabase_post.js
@@ -33,28 +33,13 @@ export async function postUserDetails() {
 export async function postDeck() {
   try {
     const id = await userId();
-    const {
-      data: { decks },
-      error: { error_decks },
-    } = await supabase
+    const { data, error } = await supabase
       .from("decks")
-      .eq("user_id", id)
-      .insert([{ name: "deck_name", description: "deck_description" }]);
-
-    const {
-      data: { categories },
-      error: { error_categories },
-    } = await supabase.from("categories").insert([{ name: "deck_category" }]);
-
-    const {
-      data: { subcategories },
-      error: { error_subcategory },
-    } = await supabase
-      .from("subcategories")
-      .insert([{ name: "deck_subcategory" }]);
-
-    if (error_decks || error_categories || error_subcategory) throw error;
-    return { decks, categories, subcategories };
+      .insert([
+        { name: "deck_name", description: "deck_description", user_id: id },
+      ]);
+    if (error) throw error;
+    return data;
   } catch (error) {
     console.log(error);
     alert(error.message);
