979c4aab525058fb84912740a6df2d1ff798db61
Armando Isaac Garcia <armando7jx@gmail.com>
2023-03-08 11:27:11 -0600

    Changed logic to use supabase helpers

--- src/pages/decks/[id]/index.js
+++ src/pages/decks/[id]/index.js
@@ -2,6 +2,7 @@ import { useRouter } from "next/router";
 import { CardContainer } from "../../../components";
 import { useState, useEffect } from "react";
 import { supabase } from "../../api/supabaseClient";
+import * as SupaHelpers from "../../api/supabase_helpers";
 
 function Decks() {
   const router = useRouter();
@@ -9,29 +10,11 @@ function Decks() {
   const [data, setData] = useState([]);
 
   useEffect(async () => {
-    const data = await loadCard();
-    console.log(data);
+    const data = await SupaHelpers.get.cardsByDeckId(id);
     if (data) {
       setData(data);
     }
   }, [id]);
-
-  const loadCard = async () => {
-    try {
-      const { data, error } = await supabase
-        .from("cards")
-        .select("id,question,answer,image,deck_id")
-        .eq("deck_id", id);
-
-      if (error) {
-        throw new Error(error.message);
-      }
-      return data;
-    } catch (error) {
-      console.error(error.message);
-      return null;
-    }
-  };
 
   return (
     <div>
