07cd0cc9a13f3467890471de9d67d23a4853d51f
jeromanp <jeduardo.roman69@gmail.com>
2023-03-07 18:31:03 -0600

    create useEffect

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -5,6 +5,21 @@ import {  getAllDeck } from "../api/supabase_helper/supabase_helper";
 function Home() {
   const [userID, setUserId] = useState("");
   const [decks, setDecks] = useState([]);
+
+  useEffect(async () => {
+    const getIdUser = async () => {
+      const user = await supabase.auth.getUser();
+      console.log(user);
+      const userID =  user.data.user.id;
+      setUserId(userID);
+    };
+
+    getIdUser();
+
+    const decks = await getAllDeck();
+    setDecks(decks);
+  }, [userID]);
+
   return (
     <>
       <h1>Esto es Home</h1>
