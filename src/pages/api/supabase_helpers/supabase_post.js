6769fec052fc5b40566453f61a1ef9f1913a4042
jeromanp <jeduardo.roman69@gmail.com>
2023-03-08 10:35:21 -0600

    created functions

--- src/pages/api/supabase_helpers/supabase_post.js
+++ src/pages/api/supabase_helpers/supabase_post.js
@@ -1,1 +1,14 @@
 import { supabase } from "../supabaseClient"
+
+
+export async function postUserDetails() {
+
+}
+
+export async function postDeck() {
+
+}
+
+export async function postCard() {
+    
+}
