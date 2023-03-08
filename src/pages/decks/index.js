446cd19f712469cb11928b9663b9b47ef9542e9c
Armando Isaac Garcia <armando7jx@gmail.com>
2023-03-08 10:45:22 -0600

    Changed to new supabase helpers

--- src/pages/decks/index.js
+++ src/pages/decks/index.js
@@ -1,39 +1,22 @@
 import * as Components from "../../components";
-import { supabase } from "../api/supabaseClient";
 import { useEffect, useState } from "react";
-import {  getAllDeck } from "../api/supabase_Helpers/supabase_Helper";
+import * as SupaHelpers from "../api/supabase_helpers";
 
 const Decks = () => {
- 
   const [userID, setUserId] = useState("");
   const [decks, setDecks] = useState([]);
 
-
-
   useEffect(async () => {
-    const getIdUser = async () => {
-      const user = await supabase.auth.getUser();
-      const userID = user.data.user.id;
-      setUserId(userID);
-    };
+    const userID = await SupaHelpers.get.userId();
+    setUserId(userID);
 
-
-    getIdUser();
-
-
-    const decks = await getAllDeck();
+    const decks = await SupaHelpers.get.userDecks();
     setDecks(decks);
   }, [userID]);
 
-
-  const deckFilter = decks.filter((e) => e.user_id === userID);
-
-  
-
-
   return (
     <div>
-      <Components.DeckContainer decks={deckFilter} />
+      <Components.DeckContainer decks={decks} />
     </div>
   );
 };
