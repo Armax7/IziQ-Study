79fbd437ca49e2f4791d9abfd061dd61557599b1
MatVaz14 <mativazquez-29@outlook.com>
2023-03-09 13:15:47 -0300

    added component SubscriptioShowcase in this route

---
+++ src/pages/currentPlan/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import * as Component from "../../components";
+
+function CurrentPlan() {
+  return <Component.SubscriptionShowcase />;
+}
+
+export default CurrentPlan;
