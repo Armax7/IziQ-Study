910e4987586d0b0daa98719a9295a89872d01930
jeromanp <jeduardo.roman69@gmail.com>
2023-03-09 13:58:36 -0600

    created function getAllUsersDetail

--- src/pages/api/users/controllers.js
+++ src/pages/api/users/controllers.js
@@ -1,1 +1,8 @@
 import { supabase } from "../supabaseClient";
+
+
+//Obtener todo de la tabla user_detail
+export const getAllUsersDetails = async () => {
+  const getAllUsers = await supabase.from("users_details").select("*");
+  return getAllUsers.data;
+};
