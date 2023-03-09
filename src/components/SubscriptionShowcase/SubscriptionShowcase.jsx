0aff8e6541af8f626ca2f987346df0178dadf830
MatVaz14 <mativazquez-29@outlook.com>
2023-03-09 13:02:59 -0300

    Modified Component structure, just leave card structure

--- src/components/SubscriptionShowcase/SubscriptionShowcase.jsx
+++ src/components/SubscriptionShowcase/SubscriptionShowcase.jsx
@@ -2,8 +2,9 @@ import style from "./SubscriptionShowcase.module.css";
 import Link from "next/link";
 import * as Chakra from "@chakra-ui/react";
 
-const SubscriptionShowcase = ({ suscription = "Premium" }) => {
-  const beneficios = [
+const SubscriptionShowcase = ({
+  suscription = "Premium",
+  benefits = [
     {
       name: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
     },
@@ -16,8 +17,8 @@ const SubscriptionShowcase = ({ suscription = "Premium" }) => {
     {
       name: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
     },
-  ];
-
+  ],
+}) => {
   return (
     <div className={style.containerGeneral}>
       <Chakra.Flex
@@ -43,7 +44,7 @@ const SubscriptionShowcase = ({ suscription = "Premium" }) => {
               </Chakra.Container>
               <Chakra.Container>
                 <Chakra.Flex flexDirection="column">
-                  {beneficios.map((beneficio) => (
+                  {benefits?.map((beneficio) => (
                     <Chakra.Flex marginBottom="14px">
                       <Chakra.Container width="min-content">
                         <Chakra.Text fontSize="26px" cursor="default">
@@ -61,92 +62,7 @@ const SubscriptionShowcase = ({ suscription = "Premium" }) => {
                   ))}
                 </Chakra.Flex>
               </Chakra.Container>
-
-              <Chakra.Flex
-                width="75%"
-                margin="auto"
-                h="40px"
-                marginBottom="10px"
-              >
-                <Chakra.Button
-                  background="#5C66BB"
-                  colorScheme="blue"
-                  fontSize="18px"
-                  borderRadius="10px"
-                  w="50%"
-                  h="100%"
-                  onClick={() => alert("Cancelar Plan 'Confirmar decisión'")}
-                >
-                  Cancel Plan
-                </Chakra.Button>
-              </Chakra.Flex>
-
-              <Chakra.Flex
-                justifyContent="flex-end"
-                width="80%"
-                margin="auto"
-                height="50px"
-              >
-                <Chakra.Button
-                  background="#5C66BB"
-                  colorScheme="blue"
-                  w="60%"
-                  h="100%"
-                  fontSize="24px"
-                  borderRadius="15px"
-                  onClick={() => alert("Mantener Plan")}
-                >
-                  Keep Plan
-                </Chakra.Button>
-              </Chakra.Flex>
             </Chakra.Flex>
-          </div>
-        </div>
-
-        <div className={style.containerTwo}>
-          <Chakra.Flex height="40%" justifyContent="flex-end">
-            <Chakra.Container
-              height="min-content"
-              width="100%"
-              display="flex"
-              justifyContent="flex-end"
-              paddingTop="25px"
-            >
-              <Link href="/home">
-                <a>
-                  <Chakra.Text fontSize="18px" color="white" fontWeight="bold">
-                    ⬅ Back to Home
-                  </Chakra.Text>
-                </a>
-              </Link>
-            </Chakra.Container>
-          </Chakra.Flex>
-          <div className={style.containerUpgrade}>
-            <Chakra.Container margin="0 auto">
-              <Chakra.Text
-                fontWeight="bold"
-                fontSize="30px"
-                textAlign="center"
-                marginBottom="20px"
-                color="black"
-                cursor="default"
-              >
-                If this plan is not for you, maybe we can offer something better
-              </Chakra.Text>
-              <Chakra.Flex w="100%">
-                <Chakra.Button
-                  margin="0 auto"
-                  fontWeight="bold"
-                  fontSize="26px"
-                  w="45%"
-                  h="60px"
-                  color="black"
-                  onClick={() => alert("Ir a la pagina de suscripciones")}
-                >
-                  Upgrade Plan
-                </Chakra.Button>
-              </Chakra.Flex>
-            </Chakra.Container>
           </div>
         </div>
       </Chakra.Flex>
