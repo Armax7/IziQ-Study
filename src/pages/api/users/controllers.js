2539286f2e9acb54a99700a7d6e8925bdd79d66c
jeromanp <jeduardo.roman69@gmail.com>
2023-03-09 13:59:09 -0600

    created function getUserDetailById

--- src/pages/api/users/controllers.js
+++ src/pages/api/users/controllers.js
@@ -6,3 +6,17 @@ export const getAllUsersDetails = async () => {
   const getAllUsers = await supabase.from("users_details").select("*");
   return getAllUsers.data;
 };
+
+
+//function que buscar user por uuid
+export const getUserDetailById = async (id) => {
+  const findUserDetailByID = await supabase
+  .from("users_details")
+  .select("*")
+  .match({ users_uuid: id })
+  .single();
+  if (!findUserDetailByID) {
+    throw Error(`No se encontró el user con ID ${id}`);
+  }
+  return findUserDetailByID.data
+};
