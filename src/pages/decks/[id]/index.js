8372b044b22dc5e2b80aa9b30c16c501adcee28b
unknown <jorgemathez14@gmail.com>
2023-03-09 15:15:12 -0300

    render and filter the car details component

--- src/pages/decks/[id]/index.js
+++ src/pages/decks/[id]/index.js
@@ -1,5 +1,5 @@
 import { useRouter } from "next/router";
-import { CardContainer } from "../../../components";
+import { CardContainer, CardDetailsContainer } from "../../../components";
 import { useState, useEffect } from "react";
 import { supabase } from "../../api/supabaseClient";
 import * as SupaHelpers from "../../api/supabase_helpers";
@@ -8,17 +8,27 @@ function Decks() {
   const router = useRouter();
   const { id } = router.query;
   const [data, setData] = useState([]);
+  const [cards, setCards] = useState([]);
+  
 
   useEffect(async () => {
     const data = await SupaHelpers.get.cardsByDeckId(id);
     if (data) {
       setData(data);
     }
+    const cards = await SupaHelpers.get.cardsByDeckId(id);
+    if (cards){
+     setCards(cards)
+    }
+
   }, [id]);
+
+
 
   return (
     <div>
       <CardContainer cards={data} />
+      <CardDetailsContainer dbCards={cards} props={cards}/>
     </div>
   );
 }
