9ef53117aaca0fc4e7c76775a7bd14930c984414
Lafsitoo <fretteleandro@live.com>
2023-03-10 02:03:52 -0300

    Added the necessary files for the component

---
+++ src/components/SubcriptionBox/SubcriptionBox.jsx
@@ -0,0 +1,8 @@
+import * as Chakra from "@chakra-ui/react";
+import { Check } from "./utils";
+
+const SubcriptionBox = () => {
+  return <Chakra.Box>Card</Chakra.Box>;
+};
+
+export { SubcriptionBox };
