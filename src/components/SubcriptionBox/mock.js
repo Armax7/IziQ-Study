215e4ef393257cbf8983fb84c2c63933949097d4
Lafsitoo <fretteleandro@live.com>
2023-03-10 21:20:09 -0300

    Add Mocks

---
+++ src/components/SubcriptionBox/mock.js
@@ -0,0 +1,23 @@
+const subscriptions = [
+    {
+      name: "Gratis",
+      description: "Accede de manera limitada a nuestros cursos y recursos.",
+    },
+    {
+      name: "Premium",
+      pricePerMonth: "$6.99",
+      pricePerYear: "$69.99",
+      description:
+        "Accede a todos nuestros cursos, recursos y sesiones de coaching personalizadas.",
+    },
+    {
+      name: "Plan Maestro",
+      pricePerMonth: "$14.99",
+      pricePerYear: "$149.99",
+      description:
+        "Acceso a todas las funciones, soporte prioritario y actualizaciones exclusivas",
+    },
+  ];
+  
+  export default subscriptions;
+  
