d0194bf6a505839f4b9b1d2c1c253281bac1b579
Lafsitoo <fretteleandro@live.com>
2023-03-11 18:11:17 -0300

    Component created

--- src/components/SubcriptionBox/SubcriptionBox.jsx
+++ src/components/SubcriptionBox/SubcriptionBox.jsx
@@ -1,8 +1,111 @@
 import * as Chakra from "@chakra-ui/react";
 import { Check } from "./utils";
+import styles from "./SubcriptionBox.module.css";
+import subscriptions from "./mock";
 
 const SubcriptionBox = () => {
-  return <Chakra.Box>Card</Chakra.Box>;
+  const subscription = subscriptions[1];
+
+  return (
+    <Chakra.Box
+      borderRadius="15px"
+      overflow="hidden"
+      p={4}
+      w={{ base: "100%", md: "345px" }}
+      minHeight="480px"
+      maxHeight="480px"
+      bg="#FFFFFF"
+      boxShadow="0px 4px 4px rgba(0, 0, 0, 0.25);"
+    >
+      {subscription.name === "Gratis" ? (
+        <Chakra.Text fontWeight="bold" fontSize="35px" mb={2} ml={1}>
+          {subscription.name}
+        </Chakra.Text>
+      ) : (
+        <Chakra.Flex>
+          <Chakra.Text fontWeight="extrabold" fontSize="32px" mb={2} ml={1}>
+            {subscription.name}
+          </Chakra.Text>
+          <Chakra.Text color="#797979" fontSize="18px" fontWeight="semibold" ml={20}>
+            1 usuario
+          </Chakra.Text>
+        </Chakra.Flex>
+      )}
+
+      <Chakra.Text fontSize="20px" fontWeight="bold" mt={3} mb={-.5} ml={1} color="#797979">
+        {subscription.pricePerMonth ? (
+          <>
+            {subscription.pricePerMonth}
+            <span className={styles.per}> por mes</span>
+          </>
+        ) : (
+          "\u200B"
+        )}
+      </Chakra.Text>
+      <Chakra.Flex></Chakra.Flex>
+      <Chakra.Text fontSize="20px" fontWeight="bold" mb={4} ml={1} color="#797979">
+        {subscription.pricePerYear ? (
+          <>
+            {subscription.pricePerYear}
+            <span className={styles.per}> por año</span>
+          </>
+        ) : (
+          "\u200B"
+        )}
+      </Chakra.Text>
+      <Chakra.Divider my="4" borderWidth="1.9px" borderRadius="4px" mb={6} mt={6}/>
+      <Chakra.Flex>
+        <Chakra.Box 
+          mt={1}
+          mr={2}
+          style={{ maxWidth: "30px", minWidth: "20px" }}
+        >
+          <Check />
+        </Chakra.Box>
+        <Chakra.Text fontSize="15px" fontWeight="medium" mb={4}>
+          {subscription.descriptionLimited}
+        </Chakra.Text>
+      </Chakra.Flex>
+      <Chakra.Flex mt={1}>
+        <Chakra.Box
+          mt={1}
+          mr={2}
+          style={{ maxWidth: "30px", minWidth: "20px" }}
+        >
+          <Check />
+        </Chakra.Box>
+        <Chakra.Text fontSize="15px" fontWeight="medium" mb={4}>
+          {subscription.descriptionFull}
+        </Chakra.Text>
+      </Chakra.Flex>
+      {subscription.name !== "Gratis" && (
+        <>
+          <Chakra.Button
+            bg="#5C66BB"
+            mt={14}
+            ml={-2}
+            py={6}
+            fontWeight="bold"
+            fontSize="20px"
+            _hover={{ bg: "#A1AAF3" }}
+            w="105%"
+            borderRadius="50px"
+            color="#FFFFFF"
+          >
+            Obtener plan
+          </Chakra.Button>
+          <Chakra.Text fontSize="12px" mt={5} ml={2} color="#8C8C8C">
+
+          {/* Añadir ruta de Términos y Condiciones */}
+
+            <Chakra.Link href="#">
+              Se aplican <span className={styles.term_cond}>Términos y Condiciones</span>
+            </Chakra.Link>
+          </Chakra.Text>
+        </>
+      )}
+    </Chakra.Box>
+  );
 };
 
 export { SubcriptionBox };
--- src/components/SubcriptionBox/mock.js
+++ src/components/SubcriptionBox/mock.js
@@ -1,23 +1,28 @@
 const subscriptions = [
-    {
-      name: "Gratis",
-      description: "Accede de manera limitada a nuestros cursos y recursos.",
-    },
-    {
-      name: "Premium",
-      pricePerMonth: "$6.99",
-      pricePerYear: "$69.99",
-      description:
-        "Accede a todos nuestros cursos, recursos y sesiones de coaching personalizadas.",
-    },
-    {
-      name: "Plan Maestro",
-      pricePerMonth: "$14.99",
-      pricePerYear: "$149.99",
-      description:
-        "Acceso a todas las funciones, soporte prioritario y actualizaciones exclusivas",
-    },
-  ];
-  
-  export default subscriptions;
-  
+  {
+    name: "Gratis",
+    descriptionLimited:
+      "Accede de manera limitada a nuestros cursos y recursos.",
+    descriptionFull:
+      "Accede a una selección limitada de nuestros cursos y recursos.",
+  },
+  {
+    name: "Premium",
+    pricePerMonth: "$6.99",
+    pricePerYear: "$69.99",
+    descriptionLimited: "Accede a nuestros cursos y recursos completos.",
+    descriptionFull:
+      "Accede a todos nuestros cursos, recursos y sesiones de coaching personalizadas.",
+  },
+  {
+    name: "Plan Maestro",
+    pricePerMonth: "$14.99",
+    pricePerYear: "$149.99",
+    descriptionLimited:
+      "Accede a nuestros cursos y recursos completos, y soporte estándar.",
+    descriptionFull:
+      "Acceso a todas las funciones, soporte prioritario y actualizaciones exclusivas.",
+  },
+];
+
+export default subscriptions;
