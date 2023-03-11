d0cb41c10dea4fe963b4aca60028b240d613dfd0
Armando Isaac Garcia <armando7jx@gmail.com>
2023-03-11 11:42:50 -0600

    Added GET by subcategory name

--- src/pages/api/subcategories/controllers.js
+++ src/pages/api/subcategories/controllers.js
@@ -5,10 +5,23 @@ export async function getSubcategoriesFromSb() {
     .from("subcategories")
     .select("id,name,category_id");
 
-    if (error){
-        console.log(error);
-        throw error;
-    }
+  if (error) {
+    console.log(error);
+    throw error;
+  }
 
-    return subcategories;
+  return subcategories;
+}
+
+export async function getSubCategoriesByName(name) {
+  const { data: subcategories, error } = await supabase
+    .from("subcategories")
+    .select("id,name,category_id")
+    .ilike("name", `%${name}%`);
+
+  if (error) {
+    console.log(error);
+    throw error;
+  }
+  return subcategories;
 }
--- src/pages/api/subcategories/handlers.js
+++ src/pages/api/subcategories/handlers.js
@@ -8,3 +8,17 @@ export async function handleGet(req, res) {
     return res.status(500).json({ error: error.message }).end();
   }
 }
+
+export async function handleGetByName(req, res) {
+  const { name } = req.query;
+
+  try {
+    const response = await Controllers.getSubCategoriesByName(name);
+    if (JSON.stringify(response) === "[]") {
+      return res.status(404).json([{ error: "Subcategory not found" }]);
+    }
+    return res.status(200).json(response);
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+}
