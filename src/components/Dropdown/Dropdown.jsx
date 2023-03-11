e9027fd9e051cf8bbda540334e2bffb3981454d0
unknown <jorgemathez14@gmail.com>
2023-03-11 17:03:39 -0300

    add properties and a new mapping

--- src/components/Dropdown/Dropdown.jsx
+++ src/components/Dropdown/Dropdown.jsx
@@ -2,17 +2,23 @@ import * as Chakra from "@chakra-ui/react";
 import * as Utils from "../../utils";
 
 function Dropdown({
-  options,
-  placeholder = "Select option...",
+  optionsC: optionsCategories,
+  placeholder = "Select Deck...",
+  placeholder2= "Select Sub..",
+  optionsSub,
+  onChangue2,
   ...props
+  
 }) {
-  if (!Array.isArray(options)) {
+  if (!Array.isArray(optionsCategories)) {
     throw new TypeError("Error: options should be an array");
   }
+  
 
     return (
+      <div>
       <Chakra.Select placeholder={placeholder} {...props} >
-        {options.map((elem, index) => {
+        {optionsCategories.map((elem, index) => {
           if (Utils.isObject(elem)) {
             return (
               <option key={index} value={elem.id}>
@@ -27,6 +33,26 @@ function Dropdown({
           )
         })}
       </Chakra.Select>
+      <Chakra.Select   placeholder={placeholder2} {...props} onChangue={onChangue2} >
+      {optionsSub.map((elem, index) => {
+          if (Utils.isObject(elem)) {
+            return (
+              <option key={index} value={elem.id}>
+                {elem.label || elem.name}
+              </option>
+            )
+          }
+          return (
+            <option key={index} value={`option${index}`}>
+              {elem}
+            </option>
+          )
+        })}
+
+        
+
+      </Chakra.Select>
+      </div>
     );
 }
 
