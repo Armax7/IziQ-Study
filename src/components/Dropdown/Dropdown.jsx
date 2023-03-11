909517d244012630a34f5ddad4a72fa763429f54
Armando Isaac Garcia <armando7jx@gmail.com>
2023-03-11 17:20:41 -0600

    Pulled Dropdown from development

--- src/components/Dropdown/Dropdown.jsx
+++ src/components/Dropdown/Dropdown.jsx
@@ -1,59 +1,29 @@
 import * as Chakra from "@chakra-ui/react";
 import * as Utils from "../../utils";
 
-function Dropdown({
-  optionsC: optionsCategories,
-  placeholder = "Select Deck...",
-  placeholder2= "Select Sub..",
-  optionsSub,
-  onChangue2,
-  ...props
-  
-}) {
-  if (!Array.isArray(optionsCategories)) {
+function Dropdown({ options, placeholder = "Select option...", ...props }) {
+  if (!Array.isArray(options)) {
     throw new TypeError("Error: options should be an array");
   }
-  
 
-    return (
-      <div>
-      <Chakra.Select placeholder={placeholder} {...props} >
-        {optionsCategories.map((elem, index) => {
-          if (Utils.isObject(elem)) {
-            return (
-              <option key={index} value={elem.id}>
-                {elem.label || elem.name}
-              </option>
-            )
-          }
+  return (
+    <Chakra.Select placeholder={placeholder} {...props}>
+      {options.map((elem, index) => {
+        if (Utils.isObject(elem)) {
           return (
-            <option key={index} value={`option${index}`}>
-              {elem}
+            <option key={index} value={elem.value || elem.id}>
+              {elem.label || elem.name}
             </option>
-          )
-        })}
-      </Chakra.Select>
-      <Chakra.Select   placeholder={placeholder2} {...props} onChangue={onChangue2} >
-      {optionsSub.map((elem, index) => {
-          if (Utils.isObject(elem)) {
-            return (
-              <option key={index} value={elem.id}>
-                {elem.label || elem.name}
-              </option>
-            )
-          }
-          return (
-            <option key={index} value={`option${index}`}>
-              {elem}
-            </option>
-          )
-        })}
-
-        
-
-      </Chakra.Select>
-      </div>
-    );
+          );
+        }
+        return (
+          <option key={index} value={`option${index}`}>
+            {elem}
+          </option>
+        );
+      })}
+    </Chakra.Select>
+  );
 }
 
 export default Dropdown;
