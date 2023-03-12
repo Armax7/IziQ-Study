2fce71fb17fce6cb7730d591d09db1e006e310d1
MatVaz14 <mativazquez-29@outlook.com>
2023-03-12 18:05:29 -0300

    The 'subscription' route was created, and as a structure the SubscriptionBoxContainer component, with two files containing info for the props

---
+++ src/pages/subscription/subscriptions.js
@@ -0,0 +1,20 @@
+let subscriptions = [
+  {
+    id: 1,
+    name: "Free",
+  },
+  {
+    id: 2,
+    name: "Premium",
+    price_per_month: "$6.99",
+    price_per_year: "$69.99",
+  },
+  {
+    id: 3,
+    name: "Maestro",
+    price_per_month: "$12.99",
+    price_per_year: "$129.99",
+  },
+];
+
+export default subscriptions;
