d79edfccd2f8a2703c5f736c8d4f46943ed754eb
Bach16 <gwyn56717@gmail.com>
2023-03-12 18:36:29 -0500

    PR front-end-49 to development

---
+++ src/components/SubcriptionBox/utils.js
@@ -0,0 +1,20 @@
+const Check = () => {
+  return (
+    <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+      <g
+        id="SVGRepo_tracerCarrier"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      ></g>
+      <g id="SVGRepo_iconCarrier">
+        <path
+          d="M19.716 4.386a1 1 0 0 1 1.572 1.236L10.665 19.136a1.5 1.5 0 0 1-2.324.042l-5.104-6.032a1 1 0 1 1 1.526-1.292l4.708 5.564L19.716 4.386z"
+          fill="#000000"
+        ></path>
+      </g>
+    </svg>
+  );
+};
+
+export { Check };
--- src/components/index.js
+++ src/components/index.js
@@ -9,6 +9,6 @@ export { default as DeckContainer } from "./DeckContainer/DeckContainer";
 export { default as LogOutButton } from "./LogOutButton/LogOutButton";
 export { default as DeckForm } from "./DeckForm/DeckForm";
 export { default as CardDetails } from "./CardDetails/CardDetails";
+export { default as SubcriptionBox } from "./SubcriptionBox/SubcriptionBox";
 export { default as CardDetailsContainer } from "./CardDetailsContainer/CardDetailsContainer";
 export { default as SubscriptionShowcase } from "./SubscriptionShowcase/SubscriptionShowcase";
-export {default as Checkout} from "./Checkout/Checkout";
