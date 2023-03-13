001476a7939c8a24c2822d5e4bde8bb917e958ae
jeromanp <jeduardo.roman69@gmail.com>
2023-03-13 12:02:57 -0600

    created fucntion postNewDeck in controllers.js

--- src/pages/api/decks/controllers.js
+++ src/pages/api/decks/controllers.js
@@ -1,23 +1,23 @@
 import { supabase } from "../supabaseClient";
 
 export const getAllDecks = async () => {
-  const getDecks = await supabase.from("decks").select("*")
-  return getDecks.data
-}
+  const getDecks = await supabase.from("decks").select("*");
+  return getDecks.data;
+};
 
 export const getDeckByUserId = async (id) => {
   const { data: getDeckByUserID, error } = await supabase
-  .from("decks")
-  .select("*")
-  .match({ user_id: id })
-if (error) {
-  throw new Error(error.message);
-}
-if (!getDeckByUserID) {
-  throw Error(`No se encontró el user con ID ${id}`);
-}
-return getDeckByUserID;
-}
+    .from("decks")
+    .select("*")
+    .match({ user_id: id });
+  if (error) {
+    throw new Error(error.message);
+  }
+  if (!getDeckByUserID) {
+    throw Error(`No se encontró el user con ID ${id}`);
+  }
+  return getDeckByUserID;
+};
 
 export const getDecksByName = async (name) => {
   const { data: getDecksByName, error } = await supabase
@@ -31,4 +31,36 @@ export const getDecksByName = async (name) => {
     throw new Error(`No se encuentran coincidencias con el Name ${name}`);
   }
   return getDecksByName;
-}
+};
+
+export const postNewDeck = async ({
+  name,
+  description = null,
+  user_id,
+  category_id,
+  subcategory_id,
+  total_cards = 0,
+  rating = 0,
+}) => {
+  const { data, error } = await supabase
+    .from("decks")
+    .insert([
+      {
+        name,
+        description,
+        status: "active",
+        category_id,
+        subcategory_id,
+        user_id,
+        total_cards,
+        rating,
+      },
+    ])
+    .select("*");
+
+  if (error) {
+    throw error;
+  }
+
+  return data;
+};
