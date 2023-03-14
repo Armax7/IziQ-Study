5648728f42b7c32a8e80c81c63305a9fd4184688
jeromanp <jeduardo.roman69@gmail.com>
2023-03-13 19:28:51 -0600

    message, deck successfully removed

--- src/pages/api/decks/handlers.js
+++ src/pages/api/decks/handlers.js
@@ -55,7 +55,7 @@ export async function deleteDecksHandler(req, res) {
 
   try {
     const response = await Controllers.removeDeckById(JSON.parse(body));
-    return res.status(200).end();
+    return res.status(200).json({ message: "Deck successfully removed" });
   } catch (error) {
     return res.status(500).json({ error: error.message });
   }
