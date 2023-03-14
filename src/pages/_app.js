d9b1d0016a13f5ee83d53643a5d62178b8f79e48
Armando Isaac Garcia <armando7jx@gmail.com>
2023-03-14 15:07:31 -0600

    Added QueryClient

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,9 +1,14 @@
 import Head from "next/head";
+import { useState } from "react";
 import { ChakraProvider } from "@chakra-ui/react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import appTheme from "../styles/theme";
 import * as Components from "../components";
 
 export default function App({ Component, pageProps }) {
+  const [queryClient] = useState(() => new QueryClient());
+
   return (
     <>
       <Head>
@@ -13,8 +18,10 @@ export default function App({ Component, pageProps }) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <ChakraProvider theme={appTheme}>
-        <Components.Navigation />
-        <Component {...pageProps} />
+        <QueryClientProvider client={queryClient}>
+          <Components.Navigation />
+          <Component {...pageProps} />
+        </QueryClientProvider>
       </ChakraProvider>
     </>
   );
