2d2a5e42b7f484137e5b7c14c9d33e7d23ade7be
jeromanp <jeduardo.roman69@gmail.com>
2023-03-15 23:12:38 -0600

    event-type case subscription deleted

--- src/pages/api/stripe-hooks.js
+++ src/pages/api/stripe-hooks.js
@@ -2,7 +2,6 @@ import initStripe from "stripe";
 import { buffer } from "micro";
 import { getServiceSupabase } from "./supabaseClient";
 
-
 export const config = { api: { bodyParser: false } };
 
 const handler = async (req, res) => {
@@ -23,12 +22,21 @@ const handler = async (req, res) => {
   const supabase = getServiceSupabase();
 
   switch (event.type) {
-    case "customer.subscription.created":
+    case "customer.subscription.updated":
       await supabase
         .from("profile")
         .update({
           is_subscribed: true,
           interval: event.data.object.items.data[0].plan.interval,
+        })
+        .eq("stripe_customer", event.data.object.customer);
+      break;
+    case "customer.subscription.deleted":
+      await supabase
+        .from("profile")
+        .update({
+          is_subscribed: false,
+          interval: null,
         })
         .eq("stripe_customer", event.data.object.customer);
       break;
