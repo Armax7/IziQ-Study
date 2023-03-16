98814c1d5faf1560962faf16b53631b5e70a3fa4
unknown <jorgemathez14@gmail.com>
2023-03-16 13:55:17 -0300

    i agre drawer in decks

--- src/pages/decks/index.js
+++ src/pages/decks/index.js
@@ -1,8 +1,14 @@
 import * as Components from "../../components";
 import { useEffect, useState } from "react";
 import * as SupaHelpers from "../api/supabase_helpers";
+import * as Chakra from "@chakra-ui/react";
+import { useDisclosure } from "@chakra-ui/react";
+
+
+
 
 const Decks = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure()
   const [userID, setUserId] = useState("");
   const [decks, setDecks] = useState([]);
 
@@ -17,6 +23,31 @@ const Decks = () => {
   return (
     <div>
       <Components.DeckContainer decks={decks} />
+
+      <Chakra.Button colorScheme='teal' onClick={onOpen}>
+        Open
+      </Chakra.Button>
+       
+        <Chakra.Drawer
+            isOpen={isOpen}
+            placement="top"
+            onClose={onClose}
+            
+            size="full"
+          >
+            <Chakra.DrawerContent>
+              <Chakra.DrawerCloseButton
+                backgroundColor="#F5E9CF"
+                color="red"
+                mr={960}
+                mt="3"
+              />
+              <Chakra.DrawerBody>
+              <Components.DeckForm />
+              </Chakra.DrawerBody>
+            </Chakra.DrawerContent>
+          </Chakra.Drawer>
+        
     </div>
   );
 };
