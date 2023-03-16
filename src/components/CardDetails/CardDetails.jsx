23585aab22f0e5d2d93a1697a21090c6f33aab00
Armando Isaac Garcia <armando7jx@gmail.com>
2023-03-15 20:49:28 -0600

    Added form when edit is pressed, added props to receive onSubmitFn and onDeleteFn

--- src/components/CardDetails/CardDetails.jsx
+++ src/components/CardDetails/CardDetails.jsx
@@ -1,90 +1,111 @@
-import * as Chakra from "@chakra-ui/react";
 import styles from "./CardDetails.module.css";
 import { HiTrash } from "react-icons/hi";
 import { RiPencilFill } from "react-icons/ri";
+import * as Chakra from "@chakra-ui/react";
+import * as ReactQuery from "@tanstack/react-query";
+import * as Components from "../../components";
+
+const QK_EDIT_MODE = "edit-mode";
 
 function CardDetails({
+  onSubmitFn = () => alert("Submit button pressed without onSubmitFn"),
+  onDeleteFn = () => alert("Delete button pressed without onDeleteFn"),
   id,
   question = "How do you say apple spanish?",
   answer = "Manzana",
-  image = "https://bit.ly/dan-abramov",
-  Deck_id,
+  image = null,
+  deck_id,
   learned,
+  ...props
 }) {
-  const onClick = (e) => {
-    return alert(`You press ${e.currentTarget.name}`);
-  };
+
   return (
-    <Chakra.Flex
+    <Chakra.Accordion
       className={styles.card}
+      allowToggle
       flex="0 0 auto"
-      padding="15px"
-      backgroundColor="white"
-      margin="20px"
-      justifyContent="space-between"
+      margin="1rem"
+      backgroundColor={"#c9ccfe"}
+      borderColor={"transparent"}
+      {...props}
     >
-      <Chakra.Flex width="80%">
-      <Chakra.Flex
-        paddingRight="50px"
-        flexDir="column"
-        borderRight="1px solid #c2c2c2"
-        width="50%"
-      >
-        <Chakra.Text color="#6E6E6E" paddingLeft="20px">
-          Question
-        </Chakra.Text>
-        <Chakra.Text paddingLeft="20px" className={styles.input}>
-          {question}
-        </Chakra.Text>
-      </Chakra.Flex>
-      <Chakra.Flex flexDir="column" width="50%">
-        <Chakra.Text color="#6E6E6E" paddingLeft="30px">
-          Answer
-        </Chakra.Text>
-        <Chakra.Text className={styles.input} paddingLeft="30px">
-          {answer}
-        </Chakra.Text>
-      </Chakra.Flex>
-      </Chakra.Flex>
-      <Chakra.Flex width="20%" justifyContent="flex-end">
-        {image && (
+      <Chakra.AccordionItem>
+        <Chakra.Flex
+          className={styles.card}
+          flex="0 0 auto"
+          justifyContent="space-between"
+          backgroundColor="white"
+          w={"100%"}
+        >
+          <Chakra.Flex width="80%">
+            <Chakra.Flex
+              paddingRight="50px"
+              flexDir="column"
+              borderRight="1px solid #c2c2c2"
+              width="50%"
+            >
+              <Chakra.Text color="#6E6E6E" paddingLeft="20px">
+                Question
+              </Chakra.Text>
+              <Chakra.Text paddingLeft="20px" className={styles.input}>
+                {question}
+              </Chakra.Text>
+            </Chakra.Flex>
+            <Chakra.Flex flexDir="column" width="50%">
+              <Chakra.Text color="#6E6E6E" paddingLeft="30px">
+                Answer
+              </Chakra.Text>
+              <Chakra.Text className={styles.input} paddingLeft="30px">
+                {answer}
+              </Chakra.Text>
+            </Chakra.Flex>
+          </Chakra.Flex>
+
           <Chakra.Image
-            boxSize="50px"
+            boxSize="4rem"
             alignSelf="center"
             borderRadius="5px"
             margin="0 5%"
             src={image}
-            alt="Card"
+            fallback={<Chakra.Box bgColor={"white"} boxSize="4rem" />}
           />
-        )}
-        <Chakra.Button
-          padding="0"
-          margin="0px 8px"
-          onClick={(e) => onClick(e)}
-          name="Edit button"
-          _focus={{ border: "none" }}
-          _hover={{ backgroundColor: "transparent" }}
-          backgroundColor="transparent"
-          size="25px"
-          h="25px"
-        >
-          <RiPencilFill size="25px" cursor="pointer" />
-        </Chakra.Button>
-        <Chakra.Button
-          padding="0"
-          margin="0px 8px"
-          onClick={(e) => onClick(e)}
-          name="Delete button"
-          _focus={{ border: "none" }}
-          _hover={{ backgroundColor: "transparent" }}
-          backgroundColor="transparent"
-          size="25px"
-          h="25px"
-        >
-          <HiTrash size="25px" cursor="pointer" />
-        </Chakra.Button>
-      </Chakra.Flex>
-    </Chakra.Flex>
+          <Chakra.Flex width="10%" justifyContent="flex-end">
+            <Chakra.AccordionButton
+              padding="0"
+              margin="0 8px"
+              name="Edit button"
+              _focus={{ border: "none" }}
+              _hover={{ backgroundColor: "transparent" }}
+              backgroundColor="transparent"
+              size="25px"
+              h="25px"
+            >
+              <RiPencilFill size="25px" cursor="pointer" />
+            </Chakra.AccordionButton>
+            <Chakra.Button
+              padding="0"
+              margin="0px 8px"
+              onClick={onDeleteFn}
+              name="Delete button"
+              _focus={{ border: "none" }}
+              _hover={{ backgroundColor: "transparent" }}
+              backgroundColor="transparent"
+              size="25px"
+              h="25px"
+            >
+              <HiTrash size="25px" cursor="pointer" />
+            </Chakra.Button>
+          </Chakra.Flex>
+        </Chakra.Flex>
+        <Chakra.AccordionPanel>
+          <Components.CardForm
+            deckId={deck_id}
+            onSubmitFn={onSubmitFn}
+            submitBtnTxt={"Save"}
+          />
+        </Chakra.AccordionPanel>
+      </Chakra.AccordionItem>
+    </Chakra.Accordion>
   );
 }
 
