d325432455c57ba328b64c98b0a6735ac915c1cb
jeromanp <jeduardo.roman69@gmail.com>
2023-03-15 19:07:15 -0600

    add user key of Stripe

--- src/pages/api/create-stripe-customer.js
+++ src/pages/api/create-stripe-customer.js
@@ -1,16 +1,17 @@
 import initStripe from "stripe";
-import { supabase } from "./supabaseClient";
+import { getServiceSupabase } from "./supabaseClient";
 
 const handler = async (req, res) => {
   if (req.query.API_ROUTE_SECRET !== process.env.API_ROUTE_SECRET) {
     return res.status(401).send("You are not authorized to call this API");
   }
-  
   const stripe = initStripe(process.env.STRIPE_SECRET_KEY);
 
   const customer = await stripe.customers.create({
     email: req.body.record.email,
   });
+
+  const supabase = getServiceSupabase();
 
   await supabase
     .from("profile")
