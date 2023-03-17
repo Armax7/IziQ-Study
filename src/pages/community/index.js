eb674a94a2d7a69f972fee9d19640f4e9c3f26f3
Armando Isaac Garcia <armando7jx@gmail.com>
2023-03-16 18:40:15 -0600

    Created /community route

---
+++ src/pages/community/index.js
@@ -0,0 +1,49 @@
+import axios from "axios";
+import * as Chakra from "@chakra-ui/react";
+import * as ReactQuery from "@tanstack/react-query";
+import * as Components from "../../components";
+import * as Utils from "../../utils";
+
+const HOST = process.env.NEXT_PUBLIC_HOST;
+const QK_TOP_SIX_DECKS = "top-six-decks";
+
+function Community() {
+  const queryClient = ReactQuery.useQueryClient();
+
+  const { data: topSixDecks } = ReactQuery.useQuery(
+    [QK_TOP_SIX_DECKS],
+    getTopSixDecks
+  );
+
+  return (
+    <Chakra.Box>
+      <Components.DeckContainer decks={topSixDecks} />
+    </Chakra.Box>
+  );
+}
+
+export async function getStaticProps() {
+  const queryClient = new ReactQuery.QueryClient();
+
+  await queryClient.prefetchQuery([QK_TOP_SIX_DECKS], getTopSixDecks);
+
+  return {
+    props: {
+      dehydratedState: ReactQuery.dehydrate(queryClient),
+    },
+  };
+}
+
+async function getTopSixDecks() {
+  const response = await axios
+    .get(`http://${HOST}/api/decks`)
+    .then((res) => res.data)
+    .then((data) =>
+      data.sort((a, b) => Utils.sortDescCompareFn(a.rating, b.rating))
+    )
+    .then((sortedData) => sortedData.slice(0, 6));
+
+  return response;
+}
+
+export default Community;
