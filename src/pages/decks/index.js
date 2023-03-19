3f3dbee93999e128a8e93e440446ba4a53eddbe8
Armando Isaac Garcia <armando7jx@gmail.com>
2023-03-18 20:00:23 -0600

    Added mutation to POST decks

--- src/pages/decks/index.js
+++ src/pages/decks/index.js
@@ -1,9 +1,15 @@
-import * as Components from "../../components";
+import axios from "axios";
 import { useEffect, useState } from "react";
-import * as SupaHelpers from "../api/supabase_helpers";
-import * as Chakra from "@chakra-ui/react";
 import { useDisclosure } from "@chakra-ui/react";
+import * as Chakra from "@chakra-ui/react";
 import { supabase } from "../api/supabaseClient";
+import * as SupaHelpers from "../api/supabase_helpers";
+import * as Components from "../../components";
+import * as ReactQuery from "@tanstack/react-query";
+
+const HOST = process.env.NEXT_PUBLIC_HOST;
+const QK_USER_ID = "user-id";
+const QK_DECKS = "decks";
 
 const Decks = () => {
   const { isOpen, onOpen, onClose } = useDisclosure();
@@ -43,6 +49,10 @@ const Decks = () => {
     setAllSubCategories(subcategories);
   }, [userID]);
 
+  const queryClient = ReactQuery.useQueryClient();
+
+  const deckFormMutation = ReactQuery.useMutation( postDeck );
+
   function filterDecksByCategory(e) {
     console.log("e.target.value", e.target.value);
 
@@ -72,50 +82,49 @@ const Decks = () => {
   }
 
   return (
-<Chakra.Box
-        borderRadius="10px"
-        fontFamily = "Poppins"
-        fontWeight="normal"
-        fontSize="25px"
-        textTransform="uppercase"
-        textAlign="center"
-      >
+    <Chakra.Box
+      borderRadius="10px"
+      fontFamily="Poppins"
+      fontWeight="normal"
+      fontSize="25px"
+      textTransform="uppercase"
+      textAlign="center"
+    >
       <Chakra.Button colorScheme="teal" onClick={onOpen}>
         Crear Mazo
       </Chakra.Button>
-      
-            
-        <h1 font="Poppins"> Filter your deck</h1>
 
-        <label> select category :</label>
-        <Components.Dropdown
-          options={[...categories]}
-          onChange={filterDecksByCategory}
-          color="black"
-          size="lg"
-          width="10%"
-          bgColor="white"
-          borderRadius="10px"
-          display="inline-block"
-          font="inherit"
-          lineHeight="center"
-          padding="2em 0.1em 2em 1em"
-        />
+      <h1 font="Poppins"> Filter your deck</h1>
 
-        <label title=""> select subcategory :</label>
-        <Components.Dropdown
-          options={[...subcategories]}
-          onChange={filterDecksBySubCategory}
-          color="black"
-          size="lg"
-          width="10%"
-          bgColor="white"
-          borderRadius="10px"
-          display="inline-block"
-          font="inherit"
-          lineHeight="center"
-          padding="2em 0.1em 2em 1em"
-        />
+      <label> select category :</label>
+      <Components.Dropdown
+        options={[...categories]}
+        onChange={filterDecksByCategory}
+        color="black"
+        size="lg"
+        width="10%"
+        bgColor="white"
+        borderRadius="10px"
+        display="inline-block"
+        font="inherit"
+        lineHeight="center"
+        padding="2em 0.1em 2em 1em"
+      />
+
+      <label title=""> select subcategory :</label>
+      <Components.Dropdown
+        options={[...subcategories]}
+        onChange={filterDecksBySubCategory}
+        color="black"
+        size="lg"
+        width="10%"
+        bgColor="white"
+        borderRadius="10px"
+        display="inline-block"
+        font="inherit"
+        lineHeight="center"
+        padding="2em 0.1em 2em 1em"
+      />
 
       <Components.DeckContainer decks={decks} />
 
@@ -133,12 +142,45 @@ const Decks = () => {
             mt="3"
           />
           <Chakra.DrawerBody>
-            <Components.DeckForm />
+            <Components.DeckForm
+              onCreateFn={deckFormMutation.mutate}
+              onCloseFn={onClose}
+            />
           </Chakra.DrawerBody>
         </Chakra.DrawerContent>
       </Chakra.Drawer>
-      </Chakra.Box>
+    </Chakra.Box>
   );
 };
+
+export async function getStaticProps() {
+  const queryClient = new ReactQuery.QueryClient();
+
+  await queryClient.prefetchQuery([QK_USER_ID], getUserID);
+  await queryClient.prefetchQuery([QK_DECKS], getUserDecks);
+
+  return {
+    props: {
+      dehydratedState: ReactQuery.dehydrate(queryClient),
+    },
+  };
+}
+
+async function getUserID() {
+  const response = await SupaHelpers.get.userId();
+  return response;
+}
+
+async function getUserDecks(userId) {
+  const response = await axios
+    .get(`http://${HOST}/api/decks/user-id/${userId}`)
+    .then((res) => res.data);
+
+  return response;
+}
+
+async function postDeck(data) {
+  return await axios.post(`http://${HOST}/api/decks`, data);
+}
 
 export default Decks;
