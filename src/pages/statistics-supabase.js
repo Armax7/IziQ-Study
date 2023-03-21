2effba69832d389ea73c0f0ed4f01a4fca856d80
jeromanp <jeduardo.roman69@gmail.com>
2023-03-20 23:40:39 -0600

    statistics supabase

---
+++ src/pages/statistics-supabase.js
@@ -0,0 +1,14 @@
+import { supabase } from "./api/supabaseClient"
+
+export function SupabaseChart() {
+  
+  return (
+    <>
+    <div>Supabase</div>   
+    </>
+
+  )
+}
+
+export default SupabaseChart
+
